3399b26335b617f12d08528977d6c320d75c3952
Jojo225consulting <consulting.josue@gmail.com>
2026-05-18 16:52:52 +0200

    replacing config by streamlit.secrets because does not work on streamlit env

--- chat_claude.py
+++ chat_claude.py
@@ -42,8 +42,8 @@ def main():
 
                 for i,conv in enumerate(old_historic): #l'envoie des prompts
                     response = client.messages.create(
-                        model = config.MODEL_NAME,
-                        max_tokens = int(config.MAX_TOKENS),
+                        model = st.secrets["MODEL_NAME"], #finalement config a été mis de coté car l'env de streamlit n'arive pas à l'utiliser normalement
+                        max_tokens = int(st.secrets["MAX_TOKENS"]),
                         system = context, 
                         messages=[
                             {"role": "user", 
